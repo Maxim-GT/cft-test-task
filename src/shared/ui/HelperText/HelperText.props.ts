a262f1cd0bbf7259601ea2e07193589d25c1ba49
Max <glukhov_maxim@mail.ru>
2026-05-07 17:00:31 +0700

    feature - добавить компоненты заголовка и подсказки

---
+++ src/shared/ui/HelperText/HelperText.props.ts
@@ -0,0 +1,5 @@
+import type { DetailedHTMLProps, HTMLAttributes } from 'react';
+
+export interface HelperTextProps extends DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement> {
+  size?: 'sm' | 'md';
+}
---
+++ src/shared/ui/HelperText/HelperText.tsx
@@ -0,0 +1,15 @@
+import cn from 'classnames';
+
+import styles from './HelperText.module.css';
+import type { HelperTextProps } from './HelperText.props';
+
+export const HelperText = ({ className, size = 'md', children, ...props }: HelperTextProps) => {
+  return (
+    <span className={cn(styles.text, className, {
+        [styles.sm]: size === 'sm',
+        [styles.md]: size === 'md',
+    })} {...props}>
+      {children}
+    </span>
+  );
+};
