24c294798040b91a3d3755b29d73c335e2dccb0a
Max <glukhov_maxim@mail.ru>
2026-05-07 17:07:22 +0700

    feature - добавить общий экспорт shared ui

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,11 @@
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Input } from './Input';
+export type { InputProps } from './Input';
+
+export { HelperText } from './HelperText';
+export type { HelperTextProps } from './HelperText';
+
+export { Title } from './Title';
+export type { TitleProps } from './Title';
