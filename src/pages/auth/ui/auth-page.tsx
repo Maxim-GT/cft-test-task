abd70a511c6f610f8387e8a3c1d0c5c58399ed24
Max <glukhov_maxim@mail.ru>
2026-05-08 15:49:11 +0700

    feature - добавить заглушки страниц авторизации и профиля

---
+++ src/pages/auth/ui/auth-page.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from 'react';
+export const AuthPage = (): JSX.Element => {
+  return (
+    <main>
+      <h1>Страница авторизации</h1>
+    </main>
+  );
+};
---
+++ src/pages/profile/ui/profile-page.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from 'react';
+export const ProfilePage = (): JSX.Element => {
+  return (
+    <main>
+      <h1>Страница профиля</h1>
+    </main>
+  );
+};
