5901ae5b6ce754c23928915ca0aa0dca72b9cb30
Max <glukhov_maxim@mail.ru>
2026-05-09 20:31:11 +0700

    fix - добавить в компонент инпута forwardRef для работы с rhf

--- src/shared/ui/Input/Input.tsx
+++ src/shared/ui/Input/Input.tsx
@@ -1,13 +1,14 @@
-import type { JSX } from 'react';
+import { forwardRef, type JSX } from 'react';
 import cn from 'classnames';
 
 import styles from './Input.module.css';
 import type { InputProps } from './Input.props';
 
-export const Input = ({ error, className, ...props }: InputProps): JSX.Element => {
+export const Input = forwardRef<HTMLInputElement, InputProps>(({ error, className, ...props }, ref): JSX.Element => {
   return (
     <div className={cn(className, styles.inputWrapper)}>
       <input
+        ref={ref}
         className={cn(styles.input, {
           [styles.error]: error,
         })}
@@ -16,4 +17,4 @@ export const Input = ({ error, className, ...props }: InputProps): JSX.Element =
       {error && <span className={styles.errorMessage}>{error}</span>}
     </div>
   );
-};
+});
