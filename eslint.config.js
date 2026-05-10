15fcdb0d287289829fffdab9b4551875dd11df89
Max <glukhov_maxim@mail.ru>
2026-05-10 15:54:11 +0700

    feature - добавить в проект prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 import js from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -6,52 +7,53 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        projectService: true,
-      },
-    },
-    rules: {
-      eqeqeq: ['error', 'always'],
-      curly: ['error', 'all'],
-      'no-debugger': 'error',
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'prefer-const': 'error',
-      'object-shorthand': ['error', 'always'],
-      '@typescript-eslint/consistent-type-imports': [
-        'error',
-        { prefer: 'type-imports' },
-      ],
-      '@typescript-eslint/no-explicit-any': 'warn',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-misused-promises': [
-        'error',
-        { checksVoidReturn: { attributes: false } },
-      ],
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_$',
-          varsIgnorePattern: '^_$',
-          caughtErrorsIgnorePattern: '^_$',
-        },
-      ],
-      'no-empty-function': 'off',
-      '@typescript-eslint/no-empty-function': 'error',
-      'no-useless-constructor': 'off',
-      '@typescript-eslint/no-useless-constructor': 'error',
-    },
-  },
+	globalIgnores(['dist']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		extends: [
+			js.configs.recommended,
+			...tseslint.configs.recommendedTypeChecked,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			eqeqeq: ['error', 'always'],
+			curly: ['error', 'all'],
+			'no-debugger': 'error',
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			'prefer-const': 'error',
+			'object-shorthand': ['error', 'always'],
+			'@typescript-eslint/consistent-type-imports': [
+				'error',
+				{ prefer: 'type-imports' },
+			],
+			'@typescript-eslint/no-explicit-any': 'warn',
+			'@typescript-eslint/no-floating-promises': 'error',
+			'@typescript-eslint/no-misused-promises': [
+				'error',
+				{ checksVoidReturn: { attributes: false } },
+			],
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{
+					argsIgnorePattern: '^_$',
+					varsIgnorePattern: '^_$',
+					caughtErrorsIgnorePattern: '^_$',
+				},
+			],
+			'no-empty-function': 'off',
+			'@typescript-eslint/no-empty-function': 'error',
+			'no-useless-constructor': 'off',
+			'@typescript-eslint/no-useless-constructor': 'error',
+		},
+	},
+	eslintConfigPrettier,
 ])
