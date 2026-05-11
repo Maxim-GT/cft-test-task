93c80e196e0e06d01f795abb2c12dc382985625c
Max <glukhov_maxim@mail.ru>
2026-05-12 00:22:16 +0700

    feature - добавить компонент инициализации сессии

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,35 @@
-import { useEffect, type JSX } from 'react';
 import { RouterProvider } from '@/app/providers/router';
 import { useSessionStore } from '@/entities/session';
-
 import { AppErrorBoundary, Loader } from '@/shared/ui';
 import { ToastProvider } from './providers/toast';
 import { ReactQueryProvider } from './providers/react-query';
+import { AppInitializer } from './providers/app-initializer';
+import type { JSX } from 'react';
 
 export function App(): JSX.Element {
 	const isHydrated = useSessionStore((state) => state.isHydrated);
 	const isLoadingSession = useSessionStore((state) => state.isLoadingSession);
 	const isAuthReady = useSessionStore((state) => state.isAuthReady);
-	const initializeAuth = useSessionStore((state) => state.initializeAuth);
 
-	useEffect(() => {
-		if (!isHydrated) {
-			return;
-		}
-		void initializeAuth();
-	}, [isHydrated, initializeAuth]);
-
-	if (isLoadingSession || !isAuthReady || !isHydrated) {
-		return <main><Loader size='lg' /></main>;
+	if (!isHydrated) {
+		return (
+			<main><Loader size='lg' /></main>
+		);
 	}
 
 	return (
 		<AppErrorBoundary>
 			<ReactQueryProvider>
-				<RouterProvider />
-				<ToastProvider />
+				<AppInitializer />
+				{(!isAuthReady || isLoadingSession) ? (
+					<main><Loader size='lg' /></main>
+				) : (
+					<>
+						<RouterProvider />
+						<ToastProvider />
+					</>
+				)}
+
 			</ReactQueryProvider>
 		</AppErrorBoundary >
 	);
---
+++ src/app/providers/app-initializer/ui/app-initializer.tsx
@@ -0,0 +1,40 @@
+import { useSessionStore } from '@/entities/session';
+import { useSessionQuery } from '@/entities/session/api/hooks/use-session-query';
+import { useEffect, type JSX } from 'react';
+
+export function AppInitializer(): JSX.Element | null {
+	const { data, isSuccess, isError } = useSessionQuery();
+	const token = useSessionStore((state) => state.token);
+
+	useEffect(() => {
+		if (!token) {
+			useSessionStore.setState({
+				isAuthReady: true,
+				isLoadingSession: false,
+				status: 'anonymous'
+			});
+			return;
+		};
+
+		if (isSuccess && data) {
+			useSessionStore.setState({
+				user: data,
+				status: 'authenticated',
+				isAuthReady: true,
+				isLoadingSession: false,
+			})
+		};
+
+		if (isError) {
+			useSessionStore.setState({
+				user: null,
+				token: null,
+				status: 'anonymous',
+				isAuthReady: true,
+				isLoadingSession: false,
+			})
+		};
+	}, [data, isSuccess, isError, token])
+
+	return null;
+}
