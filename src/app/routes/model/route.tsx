43aa6d03295d1564c26d476e4e8aa200d69eed29
Max <glukhov_maxim@mail.ru>
2026-05-11 17:48:27 +0700

    fix - оптимизация логики роутинга

--- src/app/routes/model/route.tsx
+++ src/app/routes/model/route.tsx
@@ -3,30 +3,31 @@ import { ProfilePage } from '@/pages/profile';
 import { Navigate, type RouteObject } from 'react-router-dom';
 import { PublicRoute } from '../ui/public-route';
 import { ProtectedRoute } from '../ui/protected-route';
+import { RootRedirect } from '../ui/root-redirect';
 
 export const routes: RouteObject[] = [
-    {
-        path: '/',
-        element: <Navigate to="/auth?step=phone" replace />,
-    },
-    {
-        path: '/auth',
-        element: (
-            <PublicRoute>
-                <AuthPage />
-            </PublicRoute>
-        ),
-    },
-    {
-        path: '/profile',
-        element: (
-            <ProtectedRoute>
-                <ProfilePage />
-            </ProtectedRoute>
-        ),
-    },
-    {
-        path: '*',
-        element: <Navigate to="/auth?step=phone" replace />,
-    },
+	{
+		path: '/',
+		element: <RootRedirect />,
+	},
+	{
+		path: '/auth',
+		element: (
+			<PublicRoute>
+				<AuthPage />
+			</PublicRoute>
+		),
+	},
+	{
+		path: '/profile',
+		element: (
+			<ProtectedRoute>
+				<ProfilePage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: '*',
+		element: <Navigate to="/auth?step=phone" replace />,
+	},
 ];
--- src/pages/auth/ui/auth-page.tsx
+++ src/pages/auth/ui/auth-page.tsx
@@ -3,25 +3,15 @@ import { AuthWidget, type AuthStep } from '@/widgets/auth-widget';
 import type { JSX } from 'react';
 import { Navigate, useSearchParams } from 'react-router-dom';
 import styles from './auth-page.module.css';
-import { Loader } from '@/shared/ui';
 
 export const AuthPage = (): JSX.Element => {
 	const phone = useSessionStore((state) => state.phone);
-	const status = useSessionStore((state) => state.status);
-	const isHydrated = useSessionStore((s) => s.isHydrated);
-	const isAuthReady = useSessionStore((s) => s.isAuthReady);
 	const [searchParams] = useSearchParams();
-
-	const isCodeAllowed = Boolean(phone) && status === 'otp_sent';
 
 	const rawStep = searchParams.get('step');
 	const currentStep: AuthStep = rawStep === 'code' ? 'code' : 'phone';
 
-	if (!isHydrated || !isAuthReady) {
-		return <main><Loader size='lg' /></main>;
-	}
-
-	if (currentStep === 'code' && !isCodeAllowed) {
+	if (currentStep === 'code' && !phone) {
 		return <Navigate to="/auth?step=phone" replace />;
 	}
 
