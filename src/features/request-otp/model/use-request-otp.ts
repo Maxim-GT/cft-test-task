5d454ea4aed18ced8847cf80240791e38b23250b
Max <glukhov_maxim@mail.ru>
2026-05-12 22:11:55 +0700

    feature - перенести запрос отп на react-query mutations

--- src/features/request-otp/model/use-request-otp.ts
+++ src/features/request-otp/model/use-request-otp.ts
@@ -1,42 +1,56 @@
-import { useSessionStore } from '@/entities/session';
+import { useRequestOtpMutation, useSessionStore } from '@/entities/session';
 import type { RequestOtpFormProps } from './types';
 import { useForm } from 'react-hook-form';
 import { requestOtpSchema, type RequestOtpFormData } from './schema';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { ApiError } from '@/shared/api';
+import { toast } from 'sonner';
 
 export function useRequestOtp(props: RequestOtpFormProps) {
-    const requestOtp = useSessionStore((s) => s.requestOtp);
-    const isLoading = useSessionStore((s) => s.isLoading);
-    const error = useSessionStore((s) => s.error);
-    const getMsUntilRetry = useSessionStore((s) => s.getMsUntilRetry);
-    const msUntilRetry = getMsUntilRetry();
-    const secondsLeft = Math.ceil(msUntilRetry / 1000);
-    const canResend = msUntilRetry === 0;
+	const mutation = useRequestOtpMutation();
+	const getMsUntilRetry = useSessionStore((s) => s.getMsUntilRetry);
+	const msUntilRetry = getMsUntilRetry();
+	const secondsLeft = Math.ceil(msUntilRetry / 1000);
+	const canResend = msUntilRetry === 0;
 
-    const form = useForm<RequestOtpFormData>({
-        resolver: zodResolver(requestOtpSchema),
-        defaultValues: { phone: props.initialPhone ?? '' },
-        mode: 'onSubmit',
-    });
+	const form = useForm<RequestOtpFormData>({
+		resolver: zodResolver(requestOtpSchema),
+		defaultValues: { phone: props.initialPhone ?? '' },
+		mode: 'onSubmit',
+	});
 
-    const onSubmit = form.handleSubmit(async (value) => {
-        const isOk = await requestOtp(value.phone);
+	const onSubmit = form.handleSubmit(async (value) => {
+		try {
+			await mutation.mutateAsync(value.phone);
 
-        if (!isOk) {
-            return;
-        }
+			props.onSentApiPhone?.(value);
+			props.onSentDisplayPhone?.(form.getValues('phone'));
+			props.onSuccess?.();
+		} catch (error) {
+			if (error instanceof ApiError) {
+				if (error.code === 'NETWORK_ERROR') {
+					toast.error(error.message);
+					return;
+				}
 
-        props.onSentApiPhone?.(value);
-        props.onSentDisplayPhone?.(form.getValues('phone'));
-        props.onSuccess?.();
-    });
+				form.setError('phone', {
+					type: 'server',
+					message: error.message,
+				});
 
-    return {
-        form,
-        onSubmit,
-        isLoading,
-        error,
-        secondsLeft,
-        canResend,
-    };
+				return;
+			}
+
+			toast.error('Не удалось отправить код');
+		}
+	});
+
+	return {
+		form,
+		onSubmit,
+		isLoading: mutation.isPending,
+		error: mutation.error?.message,
+		secondsLeft,
+		canResend,
+	};
 }
