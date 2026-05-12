988fe313184c64ad5e1543dae416ecff0de7e0f5
Max <glukhov_maxim@mail.ru>
2026-05-12 22:21:15 +0700

    refactor - удалить функции авторизации из стора после перезда на react-qurey mutations

--- src/entities/session/model/store.ts
+++ src/entities/session/model/store.ts
@@ -1,8 +1,6 @@
 import type { IUserDto } from '@/shared/api/auth/types';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
-import { authService } from '../api/authService';
-import { isErrorWithMessage } from '@/shared/api/errors';
 
 type AuthStatus = 'anonymous' | 'otp_sent' | 'authenticated';
 
@@ -19,14 +17,10 @@ interface ISessionState {
 	isLoadingSession: boolean;
 	isAuthReady: boolean;
 
-	requestOtp: (phone: string) => Promise<boolean>;
-	signIn: (code: number) => Promise<boolean>;
-	getSession: () => Promise<boolean>;
 	logout: () => void;
 	clearError: () => void;
 	getMsUntilRetry: () => number;
 	setIsHydrated: (value: boolean) => void;
-	initializeAuth: () => Promise<void>;
 	resetPhoneState: () => void;
 }
 
@@ -45,126 +39,6 @@ export const useSessionStore = create<ISessionState>()(
 			isLoadingSession: false,
 			isAuthReady: false,
 
-			requestOtp: async (phone) => {
-				const msUntilRetry = get().getMsUntilRetry();
-				const currentPhone = get().phone;
-
-				if (msUntilRetry > 0 && phone === currentPhone) {
-					set({
-						error: `Повторная отправка доступна через ${Math.ceil(msUntilRetry / 1000)} сек.`,
-					});
-					return false;
-				}
-
-				set({ isLoading: true, error: null });
-
-				try {
-					const data = await authService.sendOtp({ phone });
-
-					if (!data.success) {
-						set({
-							isLoading: false,
-							error: data.reason ?? 'Не удалось отправить код, попробуйте позже',
-						});
-						return false;
-					}
-
-					set({
-						phone,
-						status: 'otp_sent',
-						retryAt: Date.now() + data.retryDelay,
-						isLoading: false,
-						error: null,
-					});
-					return true;
-				} catch {
-					set({
-						isLoading: false,
-						error: 'Возникла ошибка во время отправки кода, пожалуйста, попробуйте позже',
-					});
-					return false;
-				}
-			},
-			signIn: async (code) => {
-				const phone = get().phone;
-				if (!phone) {
-					set({ error: 'Сначала запросите код' });
-					return false;
-				}
-
-				set({ isLoading: true, error: null });
-
-				try {
-					const data = await authService.signIn({ phone, code });
-
-					if (!data.success) {
-						set({
-							isLoading: false,
-							error: data.reason ?? 'Неверный код, введите другой',
-						});
-						return false;
-					}
-
-					set({
-						user: data.user,
-						token: data.token,
-						status: 'authenticated',
-						phone: null,
-						retryAt: null,
-						isLoading: false,
-						error: null,
-					});
-
-					return true;
-				} catch (err: unknown) {
-
-					set({
-						isLoading: false,
-						error: isErrorWithMessage(err) ? err.message : 'Произошла ошибка аутентификации'
-					});
-					return false;
-				}
-			},
-			getSession: async () => {
-				const token = get().token;
-				if (!token) {
-					set({ user: null, status: 'anonymous' });
-					return false;
-				}
-
-				set({ isLoading: true, error: null });
-
-				try {
-					const data = await authService.getSession();
-					if (!data.success) {
-						set({
-							user: null,
-							token: null,
-							status: 'anonymous',
-							isLoading: false,
-							error: data.reason ?? 'Даная сессия недействительна',
-						});
-						return false;
-					}
-
-					set({
-						user: data.user,
-						status: 'authenticated',
-						isLoading: false,
-						error: null,
-					});
-					return true;
-				} catch {
-					set({
-						user: null,
-						token: null,
-						status: 'anonymous',
-						isLoading: false,
-						error: 'Возникла ошибка во время проверки сессии',
-					});
-					return false;
-				}
-			},
 			logout: () => {
 				set({
 					user: null,
@@ -188,37 +62,12 @@ export const useSessionStore = create<ISessionState>()(
 				}
 				return Math.max(0, retryAt - Date.now());
 			},
-			initializeAuth: async () => {
-				if (get().isLoadingSession || get().isAuthReady) {
-					return;
-				}
-
-				set({ isLoadingSession: true });
-
-				try {
-					const { token, status } = get();
-
-					if (!token) {
-						set({
-							user: null,
-							status: status === 'otp_sent' ? 'otp_sent' : 'anonymous',
-						});
-						return;
-					}
-
-					await get().getSession();
-				} finally {
-					set({
-						isLoadingSession: false,
-						isAuthReady: true,
-					});
-				}
-			},
 
 			setIsHydrated: (value) =>
 				set({
 					isHydrated: value,
 				}),
+
 			resetPhoneState: () => {
 				set({
 					status: 'anonymous',
