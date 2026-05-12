56d68e92ba7a94f02d176d623615ac21bee13fc4
Max <glukhov_maxim@mail.ru>
2026-05-12 22:19:12 +0700

    feature - перенести проверку отп на react-query mutations

--- src/features/check-otp/model/use-check-otp.ts
+++ src/features/check-otp/model/use-check-otp.ts
@@ -1,21 +1,21 @@
-import { useSessionStore } from '@/entities/session';
+import { useRequestOtpMutation, useSessionStore, useSignInMutation } from '@/entities/session';
 import type { ICheckOtpFormProps } from './types';
 import { useForm } from 'react-hook-form';
 import { checkOtpSchema, type CheckOtpApiData, type CheckOtpFormData } from './schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useEffect, useState } from 'react';
+import { ApiError } from '@/shared/api';
+import { toast } from 'sonner';
 
 export function useCheckOtp(props: ICheckOtpFormProps) {
-	const signIn = useSessionStore((state) => state.signIn);
-	const requestOtp = useSessionStore((s) => s.requestOtp);
-	const isLoading = useSessionStore((s) => s.isLoading);
+	const signInMutation = useSignInMutation();
+	const requestOtpMutation = useRequestOtpMutation();
 	const phone = useSessionStore((state) => state.phone);
 	const getMsUntilRetry = useSessionStore((s) => s.getMsUntilRetry);
 	const msUntilRetry = getMsUntilRetry();
 	const retryAt = useSessionStore((state) => state.retryAt);
 	const secondsLeft = Math.ceil(msUntilRetry / 1000);
 	const canResend = msUntilRetry === 0 && !!phone;
-	const clearError = useSessionStore((s) => s.clearError);
 
 	const [, setTick] = useState(0);
 
@@ -41,30 +41,30 @@ export function useCheckOtp(props: ICheckOtpFormProps) {
 	});
 
 	const onSubmit = form.handleSubmit(async (value) => {
-		const isOk = await signIn(value.code);
+		try {
+			await signInMutation.mutateAsync(value.code);
+			props.onSuccess?.();
+		} catch (err) {
+			if (err instanceof ApiError) {
+				if (err.code === 'NETWORK_ERROR') {
+					toast.error(err.message);
+					return;
+				}
 
-		if (!isOk) {
-			const serverError = useSessionStore.getState().error;
+				form.setError('code', {
+					type: 'server',
+					message: err.message,
+				});
+				return;
+			}
 
-			form.setError('code', {
-				type: 'server',
-				message: serverError || 'Неверный код'
-			});
-			clearError();
-			return;
+			toast.error('Произошла ошибка авторизации');
 		}
-
-		props.onSuccess?.();
 	});
 
 	const onResend = async () => {
 		if (canResend && phone) {
-			const isOk = await requestOtp(phone);
-
-			if (!isOk) {
-				return;
-			}
-
+			await requestOtpMutation.mutateAsync(phone);
 			props.onResend?.();
 		}
 	};
@@ -73,7 +73,7 @@ export function useCheckOtp(props: ICheckOtpFormProps) {
 		form,
 		onSubmit,
 		onResend,
-		isLoading,
+		isLoading: requestOtpMutation.isPaused || signInMutation.isPending,
 		secondsLeft,
 		canResend,
 	};
